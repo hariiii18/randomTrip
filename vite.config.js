15da64f83b8659b23a341a07cd5680692840f129
aaaaaaaa <rih28184@gmail.com>
2025-05-31 22:22:00 +0900

    change

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/randomTrip/',
+  base: '/travel-picker-copy/',
   plugins: [react()],
   server: {
     proxy: {
