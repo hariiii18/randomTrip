a5d25f227086a822145917bc5d912abb4f7b9507
aaaaaaaa <rih28184@gmail.com>
2025-06-09 10:33:50 +0900

    てすと34

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -65,7 +65,7 @@ export default function Detail() {
     mission === "自由行動"
       ? `https://twitter.com/intent/tweet?text=${shareText}%0A${shareUrl}`
       : `https://twitter.com/intent/tweet?text=${shareText}%0A${encodeURIComponent(
-        `${spot}周辺で「${mission}」に挑戦します！`
+        `${spot}周辺で「${mission}」に挑戦します！ #ラントリ`
         )}%0A${shareUrl}`;
 
   return (
