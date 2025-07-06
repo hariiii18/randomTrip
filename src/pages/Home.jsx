397d916a01b1ca78474a9b4af3569a2da353a40e
aaaaaaaa <rih28184@gmail.com>
2025-07-06 14:42:42 +0900

    ラントリ段落分け

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 function Home() {
   return (
     <div style={{ textAlign: 'center', marginTop: '5rem', justifyContent: 'center'}}>
-      <h1>ようこそ！ラントリへ</h1>
+      <h1>ようこそ！<br />ラントリへ</h1>
       <p>さぁまだ見ぬ場所へ</p>
       <Link to="/select">
         <button style={{ padding: '1rem 2rem', fontSize: '1.2rem'}}>
