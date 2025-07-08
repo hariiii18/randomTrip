5458038217231fdc72893aa587cfbd0d097c8d10
aaaaaaaa <rih28184@gmail.com>
2025-07-08 09:42:39 +0900

    change name

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,10 +1,10 @@
 import { Routes, Route } from 'react-router-dom';
-import Home from './index';
-import Select from './Select';
-import Roulette from './Roulette';
-import Result from './Result';
-import Detail from './Detail';
-import Reason from './Reason';
+import Home from './index.jsx';
+import Select from './Select.jsx';
+import Roulette from './Roulette.jsx';
+import Result from './Result.jsx';
+import Detail from './Detail.jsx';
+import Reason from './Reason.jsx';
 
 export default function App() {
     return (
